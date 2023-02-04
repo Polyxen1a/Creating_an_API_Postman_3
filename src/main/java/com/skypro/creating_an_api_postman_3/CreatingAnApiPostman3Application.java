1730f55d860f02686fda72f1ab65684875e8c003
Polyxen1a <maslowa@inbox.ru>
2023-02-04 22:39:46 +0300

    Initial commit

---
+++ src/main/java/com/skypro/creating_an_api_postman_3/CreatingAnApiPostman3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.creating_an_api_postman_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingAnApiPostman3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingAnApiPostman3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/creating_an_api_postman_3/CreatingAnApiPostman3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.creating_an_api_postman_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingAnApiPostman3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
